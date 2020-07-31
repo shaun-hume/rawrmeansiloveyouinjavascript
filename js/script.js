0577e51b634459c84385bb6e71b275b87fc3ba65
Shaun Hume <s.d.hume88@gmail.com>
2020-07-31 14:42:56 +0800

    refactoring to node.js app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-function enterTheHeart()
-{
-    document.body.classList.add("transition");
+function enterTheHeart() {
+  document.body.classList.add("transition");
+  setTimeout(() => {
+    window.location.href = "mainpage.html";
+  }, 900);
+}
+
+function populateStorage() {
+  console.log("the value is set:" + localStorage.getItem("storedindb"));
+  localStorage.setItem("storedindb", Date.now());
+  document.getElementById("storageStatus").innerHTML = localStorage.getItem(
+    "storedindb"
+  );
+}
+
+function clearStorage() {
+  localStorage.clear();
+  document.getElementById("storageStatus").innerHTML = "false";
+}
+
+function loadPuzzles() {
+    // var files = fs.readdirSync('/puzzles/');
+    console.log(files);
 }
